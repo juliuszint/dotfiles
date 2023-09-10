b588307dba7749eb3d88f3f099e8339c99a0957a
juliuszint <zint.julius@gmail.com>
2023-09-10 10:41:13 +0200

    Change LSP key bindings
    Copied them in from the setup instructions. Now they do no longer add a
    delay to some of my existing shortcuts.

--- vim/ini.lua
+++ vim/ini.lua
@@ -100,10 +100,8 @@ local on_attach = function(client, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local cap_lsp = require 'cmp_nvim_lsp'.default_capabilities(vim.lsp.protocol.make_client_capabilities())
