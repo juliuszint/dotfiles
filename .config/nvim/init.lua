a37de40db4aca71aaa20d32c8ea4063a824c833d
Julius Zint <julius@zint.sh>
2026-08-08 11:44:14 +0200

    nvim: Remove LspDisable usercommand
    Replaced with built in :lsp disable command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,11 +25,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.g.mapleader = "<space>"
 vim.api.nvim_create_user_command('Vb', ':normal! <C-v>', {})
-vim.api.nvim_create_user_command('LspDisable',
-  function(_)
-    vim.lsp.stop_client(vim.lsp.get_clients())
-  end, {}
-)
 
 vim.keymap.set('n', '<S-Up>', '<C-u>')
 vim.keymap.set('n', '<S-Down>', '<C-d>')
